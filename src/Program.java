bdaffef6599fdbf9f20b7b1ba62207eea6c47a83
vinicius <viniciusbics@gmail.com>
2025-03-23 12:20:06 -0300

    First class: Position

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(4,8);
+        System.out.println(position);
     }
 }
